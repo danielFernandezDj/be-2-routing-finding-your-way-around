67e6aec700d4f649ed19156fdc6806e0390e2c7e
danielFernandezJd <mdanif9728@gmail.com>
2024-05-01 20:11:13 -0700

    install the npm .env package

--- rainbow-routes/index.js
+++ rainbow-routes/index.js
@@ -1,8 +1,34 @@
-const express = require('express')
+require('dotenv').config()
 
+const express = require('express')
 const app = express()
 
-// Set it in  home address
+// Declare routes that people can visit on the application.
 app.get('/', function (req, res) {
-    
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: white;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1>White Page</h1>
+        </body>
+    `)
 })
+
+// Color Page
+app.get('/:color', function (req, res) {
+    let myColor = req.params.color
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style="color: ${myColor};">${myColor} Page</h1>
+        </body>
+    `)
+})
+
+
+// Keep the server open.
+app.listen(process.env.PORT)
+
